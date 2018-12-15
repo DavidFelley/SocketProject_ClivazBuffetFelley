8e01d53f38951d4ca0d9de22632dfdca59d4cd23
Loris_Clivaz <Loris_Clivaz@Rislo>
2018-12-15 14:00:47 +0100

    Frame change

--- src/Server/Frame.java
+++ src/Server/Frame.java
@@ -17,10 +17,17 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Cursor;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Choice;
 
 public class Frame {
 
-	private JFrame frame;
+	private JFrame frmServer;
 	JPanel panel_1 = new JPanel();
 	
 
@@ -41,14 +48,17 @@ public class Frame {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setVisible(true);
-		frame.setBounds(100, 100, 1250, 703);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmServer = new JFrame();
+		frmServer.setTitle("SERVER");
+		frmServer.setAlwaysOnTop(true);
+		frmServer.setForeground(Color.GRAY);
+		frmServer.setVisible(true);
+		frmServer.setBounds(100, 100, 1250, 703);
+		frmServer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
-		panel.setOpaque(false);
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setBackground(Color.GRAY);
+		frmServer.getContentPane().add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
 		
 		
@@ -69,8 +79,9 @@ public class Frame {
 		JLabel lblNewLabel_1 = new JLabel("CHAT");
 		lblNewLabel_1.setForeground(Color.RED);
 		lblNewLabel_1.setBackground(Color.WHITE);
-		lblNewLabel_1.setBounds(890, 420, 56, 16);
+		lblNewLabel_1.setBounds(962, 422, 56, 16);
 		panel.add(lblNewLabel_1);
+		panel_1.setBackground(Color.GRAY);
 		
 		panel_1.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.RED, Color.RED, Color.RED, Color.RED));
 		
@@ -80,20 +91,29 @@ public class Frame {
 		
 		JLabel lblNewLabel = new JLabel("SERVER");
 		lblNewLabel.setForeground(Color.RED);
-		lblNewLabel.setBounds(887, 0, 59, 29);
+		lblNewLabel.setBounds(948, 0, 59, 29);
 		panel.add(lblNewLabel);
 		
 		
 		
 		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.GRAY);
 		panel_2.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.RED, Color.RED, Color.RED, Color.RED));
-		panel_2.setBounds(755, 451, 315, 193);
+		panel_2.setBounds(831, 451, 315, 193);
 		panel.add(panel_2);
 		
 		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(Color.GRAY);
 		panel_3.setBorder(new BevelBorder(BevelBorder.LOWERED, Color.RED, Color.RED, Color.RED, Color.RED));
-		panel_3.setBounds(29, 35, 295, 372);
+		panel_3.setBounds(30, 39, 295, 372);
 		panel.add(panel_3);
+		panel_3.setLayout(new BorderLayout(0, 0));
+		
+		Choice choice = new Choice();
+		panel_3.add(choice, BorderLayout.NORTH);
+		choice.add("loris");
+		choice.add("david");
+		choice.add("loan");
 		
 		JLabel lblNewLabel_2 = new JLabel("LIST OF FILES");
 		lblNewLabel_2.setForeground(Color.RED);
@@ -101,15 +121,15 @@ public class Frame {
 		panel.add(lblNewLabel_2);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
 		scrollPane.setBounds(822, 42, 324, 304);
 		scrollPane.setViewportView(panel_1);
 		panel.add(scrollPane);
 		
 		
 		
-		frame.repaint();
-		frame.validate();
+		frmServer.repaint();
+		frmServer.validate();
 		
 		
 	}
@@ -117,9 +137,28 @@ public class Frame {
 	public void createLabel(String message)
 	{
 		JLabel lbl = new JLabel( message);
+		
+		
 		panel_1.add(lbl);
 		
 		panel_1.validate();
 		panel_1.repaint();
+	}
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
 	}
 }
