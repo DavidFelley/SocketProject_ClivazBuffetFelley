26e4fb558aeab0bbef40b518c8c95287db668cbd
loanb <loanb@192.168.0.87>
2018-12-30 15:07:04 +0100

    renommage

--- src/Server/ServerFrame.java
+++ src/Server/ServerFrame.java
@@ -50,10 +50,10 @@ public class ServerFrame {
 		frmServer.setBounds(100, 100, 1250, 703);
 		frmServer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JPanel panelServer = new JPanel();
-		panelServer.setBackground(Color.GRAY);
-		frmServer.getContentPane().add(panelServer, BorderLayout.CENTER);
-		panelServer.setLayout(null);
+		JPanel pnlServer = new JPanel();
+		pnlServer.setBackground(Color.GRAY);
+		frmServer.getContentPane().add(pnlServer, BorderLayout.CENTER);
+		pnlServer.setLayout(null);
 
 		JButton btnPower = new JButton("ShutDown");
 		btnPower.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
@@ -62,13 +62,13 @@ public class ServerFrame {
 		
 		btnPower.addActionListener(new powerOff());
 		btnPower.setBounds(512, 587, 128, 57);
-		panelServer.add(btnPower);
+		pnlServer.add(btnPower);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(89, 11, 1046, 539);
 		scrollPane.setViewportView(pnlInfoServer);
 		scrollPane.setVisible(true);
-		panelServer.add(scrollPane);
+		pnlServer.add(scrollPane);
 		
 		
 		pnlInfoServer.setBackground(Color.GRAY);
